62ca1d4edb38593831e88f58a696d35bbd9680fc
ANJALIKRISHNA <137382104+anjalibalan1999@users.noreply.github.com>
2024-10-22 10:31:45 +0530

    All codes are added

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,18 +1,1 @@
-from django.contrib import admin
-from .models import RoomCategory, Room, Reservation, SpecialRate
 
-@admin.register(RoomCategory)
-class RoomCategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'base_price']
-
-@admin.register(Room)
-class RoomAdmin(admin.ModelAdmin):
-    list_display = ['room_number', 'category', 'is_available']
-
-@admin.register(SpecialRate)
-class SpecialRateAdmin(admin.ModelAdmin):
-    list_display = ['room_category', 'start_date', 'end_date', 'rate_multiplier']
-
-@admin.register(Reservation)
-class ReservationAdmin(admin.ModelAdmin):
-    list_display = ['room', 'start_date', 'end_date', 'customer_name', 'total_price']
