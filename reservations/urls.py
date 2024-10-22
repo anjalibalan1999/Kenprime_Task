8c4ea62c1a20a632f1126b283b444135984d67b1
ANJALIKRISHNA <137382104+anjalibalan1999@users.noreply.github.com>
2024-10-22 15:20:35 +0530

    All codes are added

--- reservations/urls.py
+++ reservations/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('availability/', views.check_availability, name='check_availability'),
-    path('reservation/', views.create_reservation, name='create_reservation'),
+    path('availability/', views.check_availability, name='check_availability'), #eg: http://127.0.0.1:8000/api/availability/?start_date=2000-11-08&end_date=2000-11-10&category=1
+    path('reservation/', views.create_reservation, name='create_reservation'),  #eg: http://127.0.0.1:8000/api/reservation/
+
 ]
